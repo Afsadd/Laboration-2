c19dc9e1a635775205f50449e44141a073dcfcdc
Afsadd <59934647+Afsadd@users.noreply.github.com>
2026-08-31 18:21:57 +0200

    Switch case
    Ändrade från elif till switch case

--- Uppgift_2.py
+++ Uppgift_2.py
@@ -1,11 +1,12 @@
 pris = [30, 28, 25, 23]
 vikt = float(input("Hur mycket väger paketet(kg): "))
 
-if vikt < 2:
-    print("Det kommer att kosta " + str(pris[0] * vikt) + " Kr")
-elif 2 <= vikt < 6:
-    print("Det kommer att kosta " + str(pris[1] * vikt) + " Kr")
-elif 6 <= vikt < 12:
-    print("Det kommer att kosta " + str(pris[2] * vikt) + " Kr")
-elif vikt >= 12:
-    print("Det kommer att kosta " + str(pris[3] * vikt) + " Kr")
+match [vikt < 2, 2 <= vikt < 6, 6 <= vikt < 12, vikt >= 12]:
+    case True, False, False, False:
+        print("Det kommer att kosta " + str(pris[0] * vikt) + " Kr")
+    case False, True, False, False:
+        print("Det kommer att kosta " + str(pris[1] * vikt) + " Kr")
+    case False, False, True, False:
+        print("Det kommer att kosta " + str(pris[2] * vikt) + " Kr")
+    case False, False, False, True:
+        print("Det kommer att kosta " + str(pris[3] * vikt) + " Kr")
